c4c017c8274eea0b820eb8e16d0484148e98dcea
Your Name <kalilpock54@gmail.com>
2024-05-03 15:03:44 -0300

    grid_viewSlots

--- lib/components/pag_inicial.dart
+++ lib/components/pag_inicial.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:fluuter_portal_celular/components/side_menu.dart';
 
 class PagInicial extends StatefulWidget {
-  const PagInicial({super.key});
+  const PagInicial({Key? key}) : super(key: key);
 
   @override
   State<PagInicial> createState() => _PagInicialState();
@@ -12,23 +12,35 @@ class PagInicial extends StatefulWidget {
 class _PagInicialState extends State<PagInicial> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Scaffold(
-        drawer: SideBarMenu(),
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 11, 89, 255),
+    return Scaffold(
+      drawer: SideBarMenu(),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        title: Text(
+          "CristalCell Portal",
         ),
-        body: Container(
-          color: const Color(0xff17203A),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: 2,
-            itemBuilder: (BuildContext context, int index) {
-              return;
-            },
+      ),
+      body: Container(
+        color: const Color(0xff17203A),
+        padding: EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
           ),
+          itemCount: 6, // Defina o número de "slots" para os celulares
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: Colors.white,
+              child: ListTile(
+                title: Text('Celular ${index + 1}'),
+                subtitle: Text('Preço: \$ ${1000 + index * 100}'),
+                // Adicione outras informações do celular aqui
+                onTap: () {
+                  // Adicione ação ao clicar no celular, se necessário
+                },
+              ),
+            );
+          },
         ),
       ),
     );
